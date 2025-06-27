f00eb61805cf12e7555117e0415923d125b177ff
Kyaw1511 <kyawminthein151193@gmail.com>
2025-06-27 15:54:26 +0700

    file a+ r+

---
+++ cProgramCodeblock/cProgramm/test36.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <conio.h>
+#include <string.h>
+#include "info.h"
+
+int main() {
+    FILE * kmt;
+    char position2[100];
+
+    kmt = fopen("test2.txt", "r+");
+    if (kmt == NULL) {
+        printf("There is no file \n\n");
+    } else {
+        printf("There is a file\n");
+    }
+
+    fscanf(kmt, "%s", position2);
+    printf("%s\n", position2);
+
+    fclose(kmt);
+
+    return 0;
+}
