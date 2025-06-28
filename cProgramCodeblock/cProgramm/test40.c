2b34b5746e5f8053c91bb5b22a9deada8fbaf227
Kyaw1511 <kyawminthein151193@gmail.com>
2025-06-28 15:48:02 +0700

    add or and xor

--- cProgramCodeblock/cProgramm/test40.c
+++ cProgramCodeblock/cProgramm/test40.c
@@ -9,14 +9,24 @@ int main() {
     unsigned int a;
     unsigned int b;
     unsigned int result;
+    unsigned int orResult;
+    unsigned int xorResult;
 
     printf("Please type a number ::: ");
     scanf("%u", &a);
     printf("Please type another number ::: ");
     scanf("%u", &b);
     result = a&b;
-    printf("The result is %u", result);
+    orResult = a|b;
+    xorResult = a^b;
+    // and operator
+    printf("The result AND operator is %u\n\n", result);
 
+    // or operator
+    printf("The result OR operator is %u\n\n", orResult);
+
+    //xor operator
+    printf("The result xor operator is %u", xorResult);
     return 0;
 
 }
