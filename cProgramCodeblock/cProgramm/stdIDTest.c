28ac8c7799ffd64bb4c87db85f585acfc5a79e65
Kyaw1511 <kyawminthein151193@gmail.com>
2025-10-10 19:28:57 +0700

    bug for StudentID_1.4

---
+++ cProgramCodeblock/cProgramm/stdIDTest.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h> // Needed for strlen()
+#include <ctype.h> // Needed for isdigit()
+
+#define MaX_ID_LENGTh 10
+
+
+// This is a testing for student ID section.
+// Because I have in trobule when the output has extra nubmer in StudentID
+// So, it is just the part of solution 
+
+int main() {
+    char input_buffer[MaX_ID_LENGTh];
+    int is_vailid = 1;
+    int i;
+    int studentID;
+
+    printf("\nEnter Student ID (digits only): \n"); 
+    //printf("Enter Student ID: ");
+    if (fgets(input_buffer, MaX_ID_LENGTh, stdin) == NULL) {
+        //printf("Error reading input. \n");
+        return 1;
+
+    }
+
+    int len = strlen(input_buffer);
+    if (input_buffer[len - 1] == '\n') {
+        len = len -1; 
+
+    }
+    
+    for (i = 0; i < len; i++) {
+        char current_char = input_buffer[i];
+        if (!isdigit(current_char)) {
+            is_vailid = 0;
+            break;
+        }
+    }
+
+    if (is_vailid) {
+        int studentId = atoi(input_buffer);
+        printf("\n Validationn passed. Stored ID: %d\n", studentID);
+
+    } else {
+        printf("\n Validation Failed! Input contains non-digit characters. \n");
+    }
+
+    int x;
+    while ((x = getchar()) != '\n' && x != EOF);
+
+    //studentID = atoi(input_buffer);
+    printf("Successfully stored ID as an integer: %d\n", studentID);
+
+    return 0;
+
+}
+
+
+
