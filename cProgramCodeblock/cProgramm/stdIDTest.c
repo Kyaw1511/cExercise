b8e23b863c40e75c29eadfe05223ea4216583d61
Kyaw1511 <kyawminthein151193@gmail.com>
2025-10-14 14:57:21 +0700

    bug for StudentID_1.4.9

--- cProgramCodeblock/cProgramm/stdIDTest.c
+++ cProgramCodeblock/cProgramm/stdIDTest.c
@@ -3,12 +3,11 @@
 #include <string.h> // Needed for strlen()
 #include <ctype.h> // Needed for isdigit()
 
-#define MaX_ID_LENGTh 10
+#define MaX_ID_LENGTh 10 // but we access only 9 digits because one digit is space for \0
 
 //bug for StudentID_1.4 (commit name);
 // This is a testing for student ID section.
-// Because I have in trobule when the output has extra nubmer in StudentID
-// So, it is just the part of solution 
+// We only access 9 digits 
 
 int main() {
     char input_buffer[MaX_ID_LENGTh];
@@ -16,76 +15,47 @@ int main() {
     int i;
     int studentID = 0;
 
+    printf("\n\nHello, Nice to meet you .... \n");
+    printf("Please Enter StudentID not over 9digit .. \n\n");
     printf("\nEnter Student ID (digits only): "); 
-    //printf("Enter Student ID: ");
+
     if (fgets(input_buffer, MaX_ID_LENGTh, stdin) == NULL) {
         printf("Error reading input. \n");
         return 1;
-
     }
 
-    //original code;
-    // int len = strlen(input_buffer);
-    // if (input_buffer[len - 1] == '\n') {
-    //     len = len -1; 
-
-    // }
-
-    // tested
     int len = strlen(input_buffer);
     if (input_buffer[len - 1] == '\n') {
         input_buffer[len - 1] = '\0';
         len = len -1;
-
     } 
-    // else {
-    //     is_valid = 0;
-    //     printf("\n Input is greater than 9 digits! Length: %d\n", len);
-    // }
-
-
-
+    
     for (i = 0; i < len; i++) {
         char current_char = input_buffer[i];
         printf("tested");
         if (!isdigit(current_char)) {
             is_valid = 0;
-            //len > MaX_ID_LENGTh; 
-            //printf("%d", is_valid);
             break;
         }
     }
 
     if (is_valid) {
-
-        if(len > MaX_ID_LENGTh - 2 ) {
-            printf("\n\nYour StudentID is over 9 digits.");
-            //return 1;
-        
-
+        if(len > 8 ) {
+            // We don't MaX_ID_LENGTh -2, 
+            // because it is the low level API in system console.
+            printf("\n\nYour StudentID is over 9 digits. \n");
+            
         } else {
             studentID = atoi(input_buffer);
-            printf("\n Validation Passed. \n");
+            printf("\n Validation Passed.... \n");
             printf("Stored ID : %d\n", studentID);
             printf("\nSuccessfully stored ID as an integer : %d\n", studentID);
-        
         } 
     } else {
         printf("\n Validation Failed!");
         printf("\n Input contains non-digit characters. \n");
-        printf("\n Your StudentID is included letter... ");
-        
+        printf("\n Your StudentID is included letter... ");    
     }
 
-//    int x;
-//    while ((x = getchar()) != '\n' && x != EOF);
-
-//     studentID = atoi(input_buffer);
-//     printf("Successfully stored ID as an integer: %d\n", studentID);
-
     return 1;
-
 }
-
-
-
